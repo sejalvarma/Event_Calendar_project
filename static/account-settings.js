ce232e153a5d77c0e41f83db01321f509e19376a
sejalvarma <sejalpv13@gmail.com>
2024-03-28 15:24:49 +0530

    added settings button confirmation popups

---
+++ static/account-settings.js
@@ -0,0 +1,13 @@
+let logout_popup = document.getElementById("logout-popup") ;
+let delEvents_popup = document.getElementById("clearEvents-popup") ;
+let delAccount_popup = document.getElementById("deleteAccount-popup") ;
+function openPopup_logout(){
+    logout_popup.classList.add("openPopup");
+}
+function openPopup_delEvents(){
+    delEvents_popup.classList.add("openPopup");
+}
+function openPopup_delAcc(){
+    delAccount_popup.classList.add("openPopup");
+}
+
